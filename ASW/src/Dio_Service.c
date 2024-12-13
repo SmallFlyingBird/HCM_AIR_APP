84974f48f934e5a740afea9de7d1c6cc6fee531e
liangwenfang <1498534720@qq.com>
2024-12-13 15:43:26 +0800

    临时代码

--- ASW/src/Dio_Service.c
+++ ASW/src/Dio_Service.c
@@ -9,62 +9,19 @@
 
 #include "Dio.h"
 #include "Dio_Service.h"
+#include "Pwm.h"
+#include "Pwm_Cfg.h"
 
-void initializePort(void){
-    /**
-     * SBC_EN:
-     * STD_HIGH: Enable
-     * STD_LOW: Disable
-    */
-    // Dio_WriteChannel(0x0090,STD_HIGH);
-    /**
-     * SBC_STB: 
-     * STD_HIGH: Normal mode or listen-only
-     * STD_LOW: GO-TO-SLEEP or Standby mode*/
-    // Dio_WriteChannel(DioConf_DioChannel_SBC_STB,STD_HIGH);
-
-    /**
-     * Motor_DrvOFF: 
-     * STD_HIGH: Disable device outputs
-     * STD_LOW: Enable device outputs*/
-    // Dio_WriteChannel(DioConf_DioChannel_MotorOut_Con_ALS, STD_HIGH);
-    /**
-     * Motor_DrvOFF: 
-     * STD_HIGH: Disable device outputs
-     * STD_LOW: Enable device outputs*/
-    // Dio_WriteChannel(DioConf_DioChannel_MotorOut_Con_AFS, STD_HIGH);
-
-    /**
-     * Limp_Con:
-     * STD_HIGH: disable BOOST limp home
-     * STD_LOW: 
-    */
-    // Dio_WriteChannel(DioConf_DioChannel_LIMP_CON,STD_HIGH);
-
-
-    /**
-     * EN_92682: 
-     * STD_HIGH: Normal mode 
-     * STD_LOW: DISABLE 92682*/
-    // Dio_WriteChannel(DioConf_DioChannel_EN_92682,STD_HIGH);
-
-        /**
-     * SPI_XXX: 
-     * STD_HIGH: SPI PCS Idle   
-     * STD_LOW: Do not set SPI PCS Low in init process
-     * */
-    Dio_WriteChannel(DioConf_DioChannel_SPI_BD_CS1,STD_HIGH);
-
-    Dio_WriteChannel(DioConf_DioChannel_SPI_BD_CS2,STD_HIGH);
-
-    // Dio_WriteChannel(DioConf_DioChannel_SPI_BD_CS3,STD_HIGH);
-
-    // Dio_WriteChannel(DioConf_DioChannel_SPI_BD_CS4,STD_HIGH);
-
-    // Dio_WriteChannel(DioConf_DioChannel_SPI_BD_CS_BOOST,STD_HIGH);
-
-    // Dio_WriteChannel(DioConf_DioChannel_CS_AFS,STD_HIGH);
-
-    // Dio_WriteChannel(DioConf_DioChannel_CS_ALS,STD_HIGH);
-
+void initializePort(void)
+{
+    Dio_WriteChannel(DioConf_DioChannel_CC_Boost_EN, STD_HIGH);
+    Dio_WriteChannel(DioConf_DioChannel_TL_Ctrl, STD_LOW); //打开TL
+    Dio_WriteChannel(DioConf_DioChannel_DRL_Ctrl, STD_LOW); //打开DRL
+    Dio_WriteChannel(DioConf_DioChannel_HSD_EN1, STD_HIGH);//HSE_EN=1 打开风扇
+    Dio_WriteChannel(DioConf_DioChannel_HSD_EN2, STD_LOW);//关电机
+    Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_H_L_Ctrl,5000,0x08000);//0x8000=100%=关闭远光；开5000 频率400HZ 占空比0
+    Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0); //拉低电机控制引脚
+    Pwm_SetDutyCycle(PwmConf_PwmChannel_PTE8_PWM_OUT, 0x3399);//0x4899U);//0x1999 约等于20%   //0x3399空载50V
 }
+
+
--- Base/CDD_OS/Os_User.c
+++ Base/CDD_OS/Os_User.c
@@ -6,7 +6,7 @@
 
 #define DEBUG_OFF 0
 #define DEBUG_ON 1
-#define LIN_AWAKE_TIME (DEBUG_OFF)//
+#define LIN_AWAKE_TIME (DEBUG_ON)//
 
 uint16 Task_Counter[OsIndex_Total];
 
@@ -17,8 +17,7 @@ void OSTask_Initial_User(void)
     // Dio_WriteChannel(DioConf_DioChannel_LIN_Wake_N, STD_LOW);
     // Dio_WriteChannel(DioConf_DioChannel_LIN_SLP_N, STD_HIGH);
 	Ex_SleepWakeupInit();
-	Dio_WriteChannel(DioConf_DioChannel_CC_Boost_EN, STD_LOW);
-
+	
 	ASW_Manager_Init(); //初始化代码
 
 	SetRelAlarm(OsIndex_5ms,1,5);
