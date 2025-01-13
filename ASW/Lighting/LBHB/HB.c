dca91aa7068421bc12f2a85b410fbe318188db3f
liangwenfang <1498534720@qq.com>
2025-01-13 13:47:43 +0800

    整合近光灯代码

--- ASW/Lighting/LBHB/HB.c
+++ ASW/Lighting/LBHB/HB.c
@@ -4,7 +4,12 @@
 #include "Buck_Interface.h"
 #include "Channel_Interface.h"
 #include "Pwm_Service.h"
+#include "Parameter_Interface.h"
 
+void HB_Init()
+{
+
+}
 void HB_On(E_ChannelID id,uint16 cur)
 {
     if(id==ChannelID1_Tap)
@@ -26,8 +31,37 @@ void HB_Off(E_ChannelID id)
     }
 }
 
+//HB运行代码
 
-void HB_RUN()
+uint16 HB_RunMainFun(E_ChannelID id,uint16 cur,uint8 SwitchOn,uint16 *sts)
 {
-
+    uint16 lgmask=0;
+    uint16 cur0=0;
+    uint8 pwmc=0;
+    lgmask=GetChannelMaskByLightFunction(E_HighBeamSpot);
+    if(((lgmask>>id)&0x01)!=0) 
+    {
+        if(SwitchOn==ACT_ON)
+        {
+            pwmc=Lighting_SetPwmRamp(id);
+            cur0=cur*pwmc/100;
+            sts[id] |= E_HB; //CH1 CH1_Tap is one channel               
+            HB_On(id,cur);
+        }
+        else
+        {             
+            sts[id] &=(~E_HB); 
+            HB_Off(id);
+        }
+        if((sts[id]&E_HB)!=0) 
+        {
+            SetLgtStsFb_HB(STS_ON);
+        }
+        else 
+        {
+            SetLgtStsFb_HB(STS_OFF);
+        }
+    }
+    return sts[id];
 }
+
--- ASW/Lighting/LBHB/LB.c
+++ ASW/Lighting/LBHB/LB.c
@@ -1,9 +1,11 @@
 
 #include "HcmPlatform.h"
 #include "LB.h"
+#include "Lighting.h"
 #include "Buck_Interface.h"
 #include "Channel_Interface.h"
 #include "Pwm_Service.h"
+#include "Parameter_Interface.h"
 
 void LB_On(E_ChannelID id,uint16 cur)
 {
@@ -26,7 +28,40 @@ void LB_Off(E_ChannelID id)
     }
 }          
 
-
+//LB运行代码
+uint16 LB_RunMainFun(E_ChannelID id,uint16 cur,uint8 SwitchOn,uint16 *sts)
+{
+    uint16 lgmask=0;
+    uint16 cur0=0;
+    uint8 pwmc=0;
+    uint16 lb_sts=0;
+    lgmask=GetChannelMaskByLightFunction(E_LowBeamKink);
+    if(((lgmask>>id)&0x01)!=0) 
+    {
+        if(SwitchOn==ACT_ON)
+        {             
+            pwmc=Lighting_SetPwmRamp(id); //get ramp pwm
+            cur0=cur*pwmc/100;  
+            sts[id] |=E_LB; //CH1 CH1_Tap会相互影响
+            LB_On(id,cur);
+        }
+        else
+        {
+            sts[id] &=(~E_LB); //CH1 CH1_Tap会相互影响
+            LB_Off(id);             
+        }
+        if((sts[id]&E_LB)!=0) 
+        {
+            SetLgtStsFb_LB(STS_ON);
+        }
+        else 
+        {
+            SetLgtStsFb_LB(STS_OFF);
+        }
+    }
+    lb_sts=sts[id];
+    return lb_sts;
+}
 
 
 
