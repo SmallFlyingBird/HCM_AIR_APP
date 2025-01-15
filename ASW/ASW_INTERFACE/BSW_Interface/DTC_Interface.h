a6d33ec5a960007a1f26f33c39e61824449d79b9
liangwenfang <1498534720@qq.com>
2025-01-15 13:54:10 +0800

    DTC_Interface

---
+++ ASW/ASW_INTERFACE/BSW_Interface/DTC_Interface.h
@@ -0,0 +1,42 @@
+
+#ifndef ASW_INTERFACE_DID_DTC_INTERFACE_DTC_INTERFACE_H_
+#define ASW_INTERFACE_DID_DTC_INTERFACE_DTC_INTERFACE_H_
+
+/****************************************************************
+ *                                                              *
+ *                     Include Files                            *
+ *                                                              *
+ ****************************************************************/
+#include "HcmPlatform.h"
+
+typedef enum
+{
+    E_CAHNNEL_OPEN = 0,
+    E_CAHNNEL_SHORT2GND = 1,
+    E_CAHNNEL_SHORT2VCC = 2,
+    E_CAHNNEL_UNVOL = 3,
+} E_ChannelErrorType;
+
+
+typedef enum
+{
+    E_BinType_Bin1 = 0,
+    E_BinType_Bin2 = 1,
+    E_BinType_Bin3 = 2,
+} E_BinType;
+
+typedef enum
+{
+    E_NtcSsignalNo_NTC1 = 0,
+    E_NtcSsignalNo_NTC2 = 1,
+    E_NtcSsignalNo_NTC3 = 2,
+    E_NtcSsignalNo_NTC4 = 3,
+    E_NtcSsignalNo_NTC5 = 4,
+} E_NtcSignalNo;
+
+void Interface_SetDtcChannelError(E_ChannelID index, E_ChannelErrorType errortype, uint8 val);
+
+
+
+#endif
+
