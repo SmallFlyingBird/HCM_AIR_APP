99c5a63f4d5c7107a38c18fd3feac06afeafd4f3
liangwenfang <1498534720@qq.com>
2025-02-12 14:15:49 +0800

    直流电机测试 波形不对

--- ASW/DCMotor/DCMotor.c
+++ ASW/DCMotor/DCMotor.c
@@ -264,49 +264,53 @@ void DCMotor_MainFunction(uint8_t timebase)
     //     DCMotor_CtrLineDtcErrDetect(); //DC_Ctrl控制线错误 设置输出的电压和DC_Ctrl的电压值有出入
     // }
     
+       // DCMotor_Run(timebase);
+    // DCMotor_StallDiagnose(); //堵转故障 
+    // DCMotor_HsdAndSigErrDetect(); //电压故障 硬件故障
+    // DCMotor_CtrLineDtcErrDetect(); //DC_Ctrl控制线错误 设置输出的电压和DC_Ctrl的电压值有出入
+
     Dio_WriteChannel(DioConf_DioChannel_HSD_EN2, STD_HIGH);//HSE_EN=1 打开电机// DCMotor_Run(timebase);
-    static uint16_t Cycle = 0;
-    static uint8_t Direction = 0;
-    // Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr, 200u, 0x8000*0.4);
+    // static uint16_t Cycle = 0;
+    // static uint8_t Direction = 0;
+    Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr, 50u, 0x8000*0.4);
 
-
-    switch( Cycle )
-    {
-        case 0u:
-            Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0);//0x4899U);//0x1999 约等于20%   //0x3399空载50V
-            break;
-        case 100u:
-            Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0x8000*0.2);
-            break;
-        case 200u:
-            Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0x8000*0.4);
-            break;
-        case 300u:
-            Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0x8000*0.6);
-            break;
-        case 400u:
-            Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0x8000*0.8);
-            break;
-        case 500u:
-            Pwm_SetDutyCycle(PwmConf_PwmChannel_DC_Ctr, 0x8000);
-            break;
-    }
-    if (Cycle == 0)
-    {
-        Direction = 0;
-    }
-    else if(Cycle == 500)
-    {
-        Direction = 1;
-    }
-    if (Direction == 0)
-    {
-        Cycle++;
-    }
-    else if (Direction == 1)
-    {
-        Cycle--;
-    }
+    // switch( Cycle )
+    // {
+    //     case 0u:
+    //         Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr, 50u,0);//0x4899U);//0x1999 约等于20%   //0x3399空载50V
+    //         break;
+    //     case 100u:
+    //         Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr,50u, 0x8000*0.2);
+    //         break;
+    //     case 200u:
+    //         Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr, 50u,0x8000*0.4);
+    //         break;
+    //     case 300u:
+    //         Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr,50u, 0x8000*0.6);
+    //         break;
+    //     case 400u:
+    //         Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr, 50u,0x8000*0.8);
+    //         break;
+    //     case 500u:
+    //         Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr,50u, 0x8000);
+    //         break;
+    // }
+    // if (Cycle == 0)
+    // {
+    //     Direction = 0;
+    // }
+    // else if(Cycle == 500)
+    // {
+    //     Direction = 1;
+    // }
+    // if (Direction == 0)
+    // {
+    //     Cycle++;
+    // }
+    // else if (Direction == 1)
+    // {
+    //     Cycle--;
+    // }
    
 }
 
