5d58564c3791bfe4d17134d24e367ef45bf729b0
liangwenfang <1498534720@qq.com>
2025-02-13 10:47:25 +0800

    H/L频率改成10KHZ  后面需要改底层，将PTA12 PTA13 支持调整为不同周期

--- ASW/src/Pwm_Service.c
+++ ASW/src/Pwm_Service.c
@@ -3,16 +3,11 @@
 #include "Pwm.h"
 #include "Pwm_Cfg.h"
 
-void Pwm_CH1Tap_Disable(void)  { 
-    // Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_H_L_Ctrl,5000,0x08000);
-    }//0x8000=100%=关闭远光；开5000 频率400HZ 占空比0
-void Pwm_CH1Tap_Enable(void)   { 
-    // Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_H_L_Ctrl,5000,0);
-    }//远光使能
+void Pwm_CH1Tap_Disable(void)  { Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_H_L_Ctrl,200,0x08000);}//0x8000=100%=关闭远光；开5000 频率400HZ 占空比0
+void Pwm_CH1Tap_Enable(void)   { Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_H_L_Ctrl,200,0);}//远光使能
 
 void Pwm_Config_DC_Ctrl(uint16 duty) { Pwm_SetPeriodAndDuty(PwmConf_PwmChannel_DC_Ctr,200,duty);}
 void PWM_Config_BOOST_TRK(void)      { Pwm_SetDutyCycle(PwmConf_PwmChannel_PTE8_PWM_OUT, 0x8000*18/100);} // TRK设置为18.5% 0x8000*18.5%=6062   18.0输出TRK=16.5%,BOOST=49V
-
 
 void Pwm_Init_All(void)
 {
