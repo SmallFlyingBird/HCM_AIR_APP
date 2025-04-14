dcfa1ae18f9e04b6609a919ccb49e187cfefdd1b
liangwenfang <1498534720@qq.com>
2025-04-14 16:15:30 +0800

    ADC7 测BOOST的温度，根据电压计算

--- ASW/ASW_INTERFACE/ADC_Interface/AdcDev_Interface.h
+++ ASW/ASW_INTERFACE/ADC_Interface/AdcDev_Interface.h
@@ -36,6 +36,7 @@ typedef enum{
 	E_AdcFunction_NTC4,
 	E_AdcFunction_NTC5,
 	E_AdcFunction_NTC6,
+	E_AdcFunction_NTC7, //等底层加//////////////////////////////////////////////////////////
 
 	E_AdcFunction_HSD1FB,
 	E_AdcFunction_DcCtr,
